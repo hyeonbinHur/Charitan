5aba8d9795743deddc56ba3590fda3057850165c
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-01-09 00:52:11 +0900

    nth

--- server/controller/donor_controller.js
+++ server/controller/donor_controller.js
@@ -1,10 +1,12 @@
 import donorService from "../service/donor_service.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const signin_donor = async (req, res) => {
   // set cache
+  console.log(process.env.DB_HOST);
   try {
     const { email } = req.body;
-
     const user = await donorService.signInUser(email);
     res.json(user);
   } catch (err) {
