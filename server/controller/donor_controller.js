73ac31cdb9a90cf307c0b81a910de71a8fd72d1a
thanhthienbui <s3878323@rmit.edu.vn>
2025-01-08 22:49:57 +0700

    completeting 2a1.4 2a1.5 and 2a1.6 but have error 500 for database test.

--- server/controller/donor_controller.js
+++ server/controller/donor_controller.js
@@ -1,11 +1,16 @@
 import donorService from "../service/donor_service.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const signin_donor = async (req, res) => {
+  console.log(process.env.DB_HOST);
   // set cache
   try {
     const { email } = req.body;
 
     const user = await donorService.signInUser(email);
+    console.log(user);
     res.json(user);
   } catch (err) {
     res.status(500).send(err.message);
