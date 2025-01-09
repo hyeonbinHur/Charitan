a11158f63fb6a1dc02fea509fa3f1d9b99d7a35c
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-01-10 01:50:45 +0900

    FEAT : Redux test done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,17 @@ import "./index.css";
 import { AuthContext } from "./context/AuthContext";
 import { ErrorProvider } from "./context/ErrorContext";
 import App from "./App";
+import { store } from "./store/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContext>
-      <ErrorProvider>
-        <App />
-      </ErrorProvider>
-    </AuthContext>
+    <Provider store={store}>
+      <AuthContext>
+        <ErrorProvider>
+          <App />
+        </ErrorProvider>
+      </AuthContext>
+    </Provider>
   </StrictMode>
 );
