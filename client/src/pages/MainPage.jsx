0f62742d24fcc6e97c59422a87b3523ab2a9fbf6
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-01-10 14:44:38 +0900

    super tired

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./MainPage.css"; // Optional: Keep for consistent styling
 
@@ -6,7 +5,9 @@ const MainPage = () => {
   const navigate = useNavigate(); // Initialize navigation function
 
   return (
-    <main style={{ padding: "20px", display: "flex", justifyContent: "center" }}>
+    <main
+      style={{ padding: "20px", display: "flex", justifyContent: "center" }}
+    >
       <div
         className="create-project-box"
         onClick={() => navigate("/create-project")} // Navigate to CreateProjectPage
